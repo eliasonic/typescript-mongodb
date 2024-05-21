1cf93270d89cb6ec89edddb9a04ce5765dfd93e8
Elias Adjetey <eliasonics@gmail.com>
2024-05-21 02:35:08 +0000

    Implemented error logging feature with Sentry

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,12 +1,19 @@
 import { Request, Response, NextFunction } from 'express'
+import * as Sentry from "@sentry/node"
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+Sentry.init({
+  dsn: "https://67130aaad87425e043613de867cfd50e@o4507269420482560.ingest.us.sentry.io/4507269718540288",
+  tracesSampleRate: 1.0
+});
+
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
     const code = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500
-    //console.log(code)
+
+    Sentry.captureException(error)
     
     res.status(code)
     res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? null : error.stack
     })
 }
